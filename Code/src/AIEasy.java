32f5479cbd404ef137aabd5f40581b274b8256da
Suhaib Ansari <suhaibansari@ku.edu>
2020-10-04 05:24:14 -0500

    little syntax error in AIEasy fixed

--- Code/src/AIEasy.java
+++ Code/src/AIEasy.java
@@ -21,7 +21,7 @@ public class AIEasy implements gameLogicInterface {
               if(!playerBoard.fleetHasSunk())
               {
                 markBoard(other, player2Printer, player1Printer);
-                return true
+                return true;
               }else
               {
                 return false;
@@ -66,7 +66,7 @@ public class AIEasy implements gameLogicInterface {
             markMiss(row, col);
         }
       }
-      opbaord.print(true);
+      opboard.print(true);
       playerboard.print(true);
     }
 
