af90195139559297997e5fbc52c3e9cb28c5df1b
Brian Cheung <briancheung@ku.edu>
2020-10-04 19:25:10 -0500

    tried fixing ai easy

--- Code/src/AIEasy.java
+++ Code/src/AIEasy.java
@@ -3,12 +3,10 @@ import java.util.Random;
 public class AIEasy implements gameLogicInterface {
     private int row;
     private int col;
-    private Board BoardCopy;//this is going to keep track what's been visited
     private Board BoardOrig;//the actual board pointer
     Random rand = new Random();//RNG for placing ships and for firing initially
 
     AIEasy(Board orig){
-        this.BoardCopy = orig.getCopyBoard(orig);
         this.BoardOrig = orig;
     }
 
@@ -28,10 +26,6 @@ public class AIEasy implements gameLogicInterface {
               }
     }
 
-    private void markHit(int row, int col){
-        BoardCopy.addMarker('x', row, col);
-    }
-
     private void markOrig(int row, int col){
         BoardOrig.addMarker('x', row, col);
     }
@@ -41,13 +35,15 @@ public class AIEasy implements gameLogicInterface {
     }
 
     private boolean markRandom(int size) {
-        row = rand.nextInt(size);
-        col = rand.nextInt(size);
-        return isHit(row, col, 's');
+      do {
+          row = rand.nextInt(size);
+          col = rand.nextInt(size);
+      }while(origHit(row, col, 'o'));
+      return isHit(row, col, 's');
     }
 
     private boolean isHit(int row, int col, char letter) {
-        if(BoardCopy.getMarker(row, col) == letter){
+        if(BoardOrig.getMarker(row, col) == letter){
             return true;
         } else {
             return false;
@@ -55,17 +51,16 @@ public class AIEasy implements gameLogicInterface {
     }
 
     public void markBoard(Board opponent, BoardPrinterWrapper opboard, BoardPrinterWrapper playerboard){
-      if(!isHit(row, col, 'x'))
-      {
-        if(markRandom(opponent.getXSize()))
+      if(markRandom(opponent.getXSize()))
         {
-            markHit(row, col);
+          if(!isHit(row, col, 'x') && !isHit(row, col, 'o'))
+          {
             markOrig(row, col);
+          }
         }else
         {
             markMiss(row, col);
         }
-      }
       opboard.print(true);
       playerboard.print(true);
     }
